b5a049ff2fab31964de977ac85a7c9b47303170b
guillaume.letreut <guillaume.letreut@gmail.com>
2019-07-02 12:59:43 +0200

    common

--- pymol/show_trajectory.py
+++ pymol/show_trajectory.py
@@ -1,6 +1,7 @@
 ############################################################################
 ## imports
 ############################################################################
+#from __future__ import print_function
 from pymol.cgo import *
 from pymol import cmd, stored
 import os,sys,shutil
@@ -21,13 +22,21 @@ from boundingbox import *
 ## global parameters
 ############################################################################
 prefix='state'
-quit=False
+makemovie=True
+nmin=0
+nmax=1000
+fps=20
+quit=True
+
+nc=80       # index of the centromer
+view=None
+#view="-0.467148870,0.704388320,-0.534410000,0.881349385,0.322722554,-0.345051497,-0.070578903,-0.632196784,-0.771575153,0.000000000,0.000000000,-164.520812988,-2.193372726,-3.725057602,-2.012924194,129.709457397,199.332168579,20.000000000" # scale 1
 
 ############################################################################
 ## main script
 ############################################################################
 if (len(sys.argv) != 2):
-    print ("syntax: <.xyz directory>")
+    print ("syntax: <trajectory.xyz>")
     sys.exit()
 
 # trajectory directory
@@ -39,8 +48,15 @@ print ("{:<20s}{:<s}".format("traj dir",trajdir))
 # load states in trajectory
 load_xyz_states(trajdir, prefix=prefix)
 
+# set view
+if not view is None:
+    set_view_states(view)
+
 # color first 3 monomers
-color_123()
+#color_123()
+
+# emphasize the centromere region
+emphasize_centromer(nc)
 
 # draw box
 #if ( ('Lx' in params) and ('Ly' in params) and ('Lz' in params) ):
@@ -49,6 +65,9 @@ color_123()
 #    Ly=float(params['Ly'][0])
 #    Lz=float(params['Lz'][0])
 #    drawBox(-Lx/2.,-Ly/2.,-Lz/2.,Lx/2.,Ly/2.,Lz/2., r=0.5,g=0.5,b=0.5)
+
+if (makemovie):
+    make_movie(npts=1001,nmin=nmin,nmax=nmax, imgpx_w=800, imgpx_h=600, framerate=fps)
 
 if (quit):
     cmd.quit()
